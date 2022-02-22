c52d1ad4ba9c0692738d7b3bcad27226fc859010
sangram-mkj <sangrammukherjee90@gmail.com>
2022-02-23 01:22:48 +0530

    Added Navbar & Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Navbar } from './components/Navbar/Navbar';
+import { Footer } from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
+     <Footer />
+
     </div>
   );
 }
