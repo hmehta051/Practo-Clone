38b7cea007dd9f0999b039b14f0dccafd2560f02
Suryansh Pajiala <63901097+Sunny9899@users.noreply.github.com>
2022-02-23 00:48:59 +0530

    Main  Page almost done

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,19 +1,42 @@
 import "../styles/MainPage.css";
-//
+import {Link} from "react-router-dom";
+
 export const MainPage = () =>{
     return <div className="main-container">
     Navbar
      <div className="second-container">
 
-         <div id="third-container-top">
-        <div>Login</div> 
-         <div>Register</div>
+         <div className="third-container-top">
+        <Link to="/login"><span>Login</span></Link> 
+         <Link to="/signup"><span>Register</span></Link>
          </div>
          <hr/>
          <div id="third-container-body">
              <div id="imgOnLeft"><img src="https://accounts.practo.com/static/images/illustration.png" width="400px"/></div>
-             <div id="formOnLeft">
+             <div id="formOnRight">
                  
+             <div className="Heading">
+                <h4>
+                    Join Practo
+                </h4>
+                <p>Are you a doctor? <a className="here">Register Here</a></p>
+            </div>
+            <hr className="Line"/>
+            <br/>
+            <div id="inp">Full Name</div>
+            <input type="text" name="name"  placeholder="Full Name" />
+            <div id="inp">Mobile Number</div>
+            <input type="text" name="mobile"  placeholder="Mobile Number"  />
+            <div id="inp">Create Password</div>
+            <input type="password" name="password"  placeholder="Create password" />
+            <br/>
+            <br/>
+            <div className="check">
+                <input type="checkbox" /><label>Receive relevant offers and promotional communication from practo</label>
+            </div>
+            <p className="smallp">By signing up, I agree to <a className="smalla">terms</a></p>
+            <div className="button">Register</div>
+
              </div>
          </div>
 
