157cd0bf48ad6f129f0ffcac75b75da21ee56474
sangram-mkj <sangrammukherjee90@gmail.com>
2022-02-26 00:12:56 +0530

    Incomplete homepage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
+import { HomePage } from './pages/homepage/HomePage';
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <HomePage />
      <Footer />
 
     </div>
