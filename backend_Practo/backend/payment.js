63f6d1ed160da175eefb24f7374cb9816eefc699
adityaprasad03cvcmsd <91816884+adityaprasad03cvcmsd@users.noreply.github.com>
2022-02-27 18:28:43 +0530

    update

---
+++ backend_Practo/backend/payment.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const path = require('path')
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app=express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const razorpay = new Razorpay({
+	key_id: 'rzp_test_9vxvaS7bgzC98i',
+	key_secret: 'X6k6UFwxWVAnplR4cQcsz24Y'
+})
+
+app.get('/logo.svg', (req, res) => {
+	res.sendFile(path.join(__dirname, 'logo.svg'))
+})
+
+app.post('/verification', (req, res) => {
+	// do a validation
+	const secret = '12345678'
+
+	console.log(req.body)
+
+	const crypto = require('crypto')
+
+	const shasum = crypto.createHmac('sha256', secret)
+	shasum.update(JSON.stringify(req.body))
+	const digest = shasum.digest('hex')
+
+	console.log(digest, req.headers['x-razorpay-signature'])
+
+	if (digest === req.headers['x-razorpay-signature']) {
+		console.log('request is legit')
+		// process it
+		require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+	} else {
+		// pass it
+	}
+	res.json({ status: 'ok' })
+})
+
+app.post('/razorpay', async (req, res) => {
+	const payment_capture = 1
+	const amount = 499
+	const currency = 'INR'
+
+	const options = {
+		amount: amount * 100,
+		currency,
+		receipt: shortid.generate(),
+		payment_capture
+	}
+
+	try {
+		const response = await razorpay.orders.create(options)
+		console.log(response)
+		res.json({
+			id: response.id,
+			currency: response.currency,
+			amount: response.amount
+		})
+	} catch (error) {
+		console.log(error)
+	}
+})
+
+app.listen(9000, () => {
+	console.log('Listening on 9000')
+})
